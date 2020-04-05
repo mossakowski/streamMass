a648be1424c82e01f3e9f2da4b868de2de2ccda0
Mossakowski Kamil <mosakamil@gmail.com>
2020-04-05 21:12:10 +0200

    correct name of statements, fuction

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var tab_id = ['tab_mass_weekday_saturday', 'tab_mass_friday', 'tab_mass_sunday'];
-
 var tab_mass_weekday_saturday = ['07:00', '08:00', '18:00'];
 var tab_mass_friday = ['07:00', '08:00', '16:30', '18:00'];
 var tab_mass_sunday = ['07:00', '08:30', '10:00', '11:30', '16:30', '19:30'];
@@ -7,11 +5,10 @@ var tab_mass_sunday = ['07:00', '08:30', '10:00', '11:30', '16:30', '19:30'];
 const formatTime = "HH:mm";
 const formatDay = 'd';
 
-var currentTime = moment('08:00',formatTime);
-var currentDay = 0;
+var currentTime = moment('04:00',formatTime);
+var currentDay = 4; // moment().format('d');
 
-function selectTab() {
-     // moment().format('d'); jakbyś wprowadził skrypt w życie :)
+function selectTab(currentDay) {
     if(currentDay >= 1 && currentDay <=4 || currentDay == 6) {
         return tab = tab_mass_weekday_saturday;
     }
@@ -25,13 +22,9 @@ function selectTab() {
     }
 }
 
-
-
-//spawdź czy jest aktualnie transmisja
 function checkIsMassNow() {
     var i = 0;    
-    selectTab();
-    tab.forEach(function(items) {
+    selectTab(currentDay).forEach(function(items) {
         var items = moment(items, formatTime);
         var startStream = moment(items, formatTime)
                 .subtract(10, 'm');
@@ -46,15 +39,11 @@ function checkIsMassNow() {
     if(i>0) {
         displayStream();
     } else {
-        checkWhenNextMass(tab);
+        checkWhenNextMass(selectTab(currentDay));
     }
 }
 
-//Sprawdź kiedy jest następna msza
 function checkWhenNextMass(tab) {
-   //1.Dodaj do obecnego dnia plus jeden a następnie sprawdź, pierwszy element tablicy i zwróć
-    // var currentTime = moment('13:21',formatTime);
-    // var currentDay = 0 // moment().format('d'); jakbyś wprowadził skrypt w życie :)
     var tab_next_mass = [];
     var i = 0;
     tab.forEach(function(items) {
@@ -65,7 +54,7 @@ function checkWhenNextMass(tab) {
     });
 
     if(i === 0) {
-      var nextDay = moment('0', formatDay).add(1,'d').format(formatDay);
+      let nextDay = moment(currentDay, formatDay).add(1,'d').format(formatDay);
       nextDay = parseInt(nextDay);
         console.log("Zapraszamy na jutrzejszą msze o: " + selectTab(nextDay)[0]);
         
@@ -73,9 +62,6 @@ function checkWhenNextMass(tab) {
       console.log("Następna msza odbędzie się o: " + tab_next_mass[0]);
     }    
 }
-
-console.log('test fetch');
-
 
 function displayStream() {
     console.log('Tu będzie iframe, jest transmisja!');
