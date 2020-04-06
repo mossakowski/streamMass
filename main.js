57ac73a7c8efa617ba6ae789cbf772a94812f5f5
Mossakowski Kamil <mosakamil@gmail.com>
2020-04-06 23:26:33 +0200

    add schedule mass

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-var tab_mass_weekday_saturday = ['07:00', '08:00', '18:00'];
-var tab_mass_friday = ['07:00', '08:00', '16:30', '18:00'];
-var tab_mass_sunday = ['07:00', '08:30', '10:00', '11:30', '16:30', '19:30'];
+var tab_mass_schedule = {
+    tab_mass_weekday_saturday : ['07:00', '08:00', '18:00'],
+    tab_mass_friday : ['07:00', '08:00', '16:30', '18:00'],
+    tab_mass_sunday : ['07:00', '08:30', '10:00', '11:30', '16:30', '19:30']
+}
 
 const screen = document.getElementById('screen');
 
-const formatTime = "HH:mm";
-const formatDay = 'd';
+const format_time = "HH:mm";
+const format_day = 'd';
 
 function app() {
+    
     class momentDateTime {
         
         currentTime() {
-            return moment(moment().format(formatTime),formatTime);
+            return moment(moment().format(format_time),format_time);
         }
         
         currentDay() { 
@@ -19,35 +22,35 @@ function app() {
         }
     }
 
-    var objDateTime = new momentDateTime();
+    var obj_date_time = new momentDateTime();
 
-    var currentDay = objDateTime.currentDay();
-    var currentTime = objDateTime.currentTime();
+    var current_day = obj_date_time.currentDay();
+    var current_time = obj_date_time.currentTime();
 
-    function selectTab(currentDay) {
-        if(currentDay >= 1 && currentDay <=4 || currentDay == 6) {
-            return tab = tab_mass_weekday_saturday;
+    function selectTab(current_day) {
+        if(current_day >= 1 && current_day <=4 || current_day == 6) {
+            return tab = tab_mass_schedule['tab_mass_weekday_saturday'];
         }
 
-        if(currentDay === 5) {
-            return tab = tab_mass_friday;
+        if(current_day === 5) {
+            return tab = tab_mass_schedule['tab_mass_friday'];
         }
 
-        if(currentDay === 0) {
-            return tab = tab_mass_sunday;
+        if(current_day === 0) {
+            return tab = tab_mass_schedule['tab_mass_sunday'];
         }
     }
 
     function checkIsMassNow() {
         var i = 0;    
-        selectTab(currentDay).forEach(function(items) {
-            var items = moment(items, formatTime);
-            var startStream = moment(items, formatTime)
+        selectTab(current_day).forEach(function(items) {
+            var items = moment(items, format_time);
+            var start_stream = moment(items, format_time)
                     .subtract(10, 'm');
-            var endStream = moment(items, formatTime)
+            var end_stream = moment(items, format_time)
                     .add(70, 'm');
                     
-            if(currentTime.isBetween(startStream,endStream)) {
+            if(current_time.isBetween(start_stream,end_stream)) {
                 i++;
             }
         });
@@ -55,7 +58,7 @@ function app() {
         if(i>0) {
             showStream();
         } else {
-            checkWhenNextMass(selectTab(currentDay));
+            checkWhenNextMass(selectTab(current_day));
         }
     }
 
@@ -63,16 +66,16 @@ function app() {
         var tab_next_mass = [];
         var i = 0;
         tab.forEach(function(items) {
-            if(currentTime.isBefore(moment(items,formatTime))) {
+            if(current_time.isBefore(moment(items,format_time))) {
                 tab_next_mass[i] = items;
                 i++;
             }
         });
 
         if(i === 0) {
-        let nextDay = moment(currentDay, formatDay).add(1,'d').format(formatDay);
-        nextDay = parseInt(nextDay);
-            console.log("Zapraszamy na jutrzejszą msze o: " + selectTab(nextDay)[0]);
+        let next_day = moment(current_day, format_day).add(1,'d').format(format_day);
+        next_day = parseInt(next_day);
+            console.log("Zapraszamy na jutrzejszą msze o: " + selectTab(next_day)[0]);
             hideStream();
         } else {
         console.log("Następna msza odbędzie się o: " + tab_next_mass[0]);
@@ -95,6 +98,18 @@ function app() {
 }
 
 app();
+
+(function() {
+    var mass_schedule_container = document.querySelector('.mass_schedule');
+    var mass_schedule_children = mass_schedule_container.querySelectorAll('[data-mass]');
+
+    for(let i=0;i<mass_schedule_children.length;i++) {
+        let tab = 'tab_mass_' + mass_schedule_children[i].getAttribute('data-mass');
+        let text = tab_mass_schedule[tab].join(' ');
+        mass_schedule_children[i].innerHTML = text;
+    }
+})();
+
 setInterval(function() {
     app();
 }, 60 * 1000);
